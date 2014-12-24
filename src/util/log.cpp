1e51ae49a2769ba6f1d9244a6463d8d846694907
blargg <anuibis@sbcglobal.net>
2014-12-24 14:10:48 -0600

    multiple log levels

--- src/util/log.cpp
+++ src/util/log.cpp
@@ -1,3 +1,4 @@
 #include "util/log.h"
 
 FILE *LOG_file = stderr;
+int LOG_level = 0;
--- src/util/log.h
+++ src/util/log.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 extern FILE *LOG_file;
+extern int LOG_level;
 
 #define NO_OP ((void) 0)
 
@@ -11,12 +12,29 @@ extern FILE *LOG_file;
 // for when we want logging
 
 #define LOG(fmt, args...) fprintf(LOG_file, "%s:%d ==> " fmt "\n", __FILE__, __LINE__, ##args)
+
+#define LOG_(type, fmt, args...) \
+{fprintf(LOG_file, type ": %s:%d ==> " fmt "\n", __FILE__, __LINE__, ##args); }
+
+#define LOG_E(fmt, args...) {if (4 >= LOG_level) { LOG_("ERROR", fmt, ##args); }}
+#define LOG_W(fmt, args...) {if (3 >= LOG_level) { LOG_("WARNING", fmt, ##args); }}
+#define LOG_I(fmt, args...) {if (2 >= LOG_level) { LOG_("INFO", fmt, ##args); }}
+#define LOG_D(fmt, args...) {if (1 >= LOG_level) { LOG_("DEBUG", fmt, ##args); }}
+#define LOG_T(fmt, args...) {if (0 >= LOG_level) { LOG_("TRACE", fmt, ##args); }}
+
 #define LOG_IF(bool_exp, fmt, args...) {if(bool_exp) { LOG(fmt, ##args); }}
 
 #else
 // for when we don't want logging
 
 #define LOG(...) NO_OP
+
+#define LOG_E(fmt, args...) NO_OP
+#define LOG_W(fmt, args...) NO_OP
+#define LOG_I(fmt, args...) NO_OP
+#define LOG_D(fmt, args...) NO_OP
+#define LOG_T(fmt, args...) NO_OP
+
 #define LOG_IF(...) NO_OP
 
 #endif
